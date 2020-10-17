1cb433822cd319e2d82b5ae4f5dbafee3bb5e182
Raven D Co Seng <ravenione.coseng.iics@ust.edu.ph>
2020-10-17 16:43:19 +0800

    added condition checking

--- src/proglang/ProgLang.java
+++ src/proglang/ProgLang.java
@@ -21,13 +21,14 @@ public class ProgLang {
 
     //list of words
     public static LinkedList<Word> codelist = new LinkedList<Word>();
+    public static LinkedList<Word> conditionCodelist = new LinkedList<Word>();
     //for checking of braces, bracket,parenthesis
     public static Deque<Character> charstack = new ArrayDeque<Character>();
     
     public static void main(String[] args) throws Exception {
         //String to be tested
         String code;
-        
+        //do { String w = "haha" ; System.out.println ( w ) ; } while ( ( x < 10 ) || ( isRound ) && ( x >= 20 ) ) ;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter code");
         code = scanner.nextLine();
@@ -56,13 +57,23 @@ public class ProgLang {
             if (codelist.get(i).type == null) {
                 regexChecker(codelist.get(i));
             }
-
         }
-        for (int i = 0; i < codelist.size(); i++) {
+        
+        boolean afterWhile = false;
+        
+        for (int i = 0; i < codelist.size(); i++) {            
             //pang check kung tama yung naassign HAHA
-            System.out.println(codelist.get(i).word + " -> " + codelist.get(i).type);
-            
-        }                          
+            System.out.println(codelist.get(i).word + " -> " + codelist.get(i).type);            
+            if (codelist.get(i).word.equals("while")) {
+                afterWhile = true;
+            }
+            if (afterWhile && !codelist.get(i).word.equals("(") && !codelist.get(i).word.equals(")")) {
+                conditionCodelist.add(codelist.get(i)); 
+            }
+        }
+        
+        checkCondition(conditionCodelist);   
+        
     }
 
     public static void isBalanced(String s) {
@@ -163,6 +174,12 @@ public class ProgLang {
             case "int":
                 w.type = "intDataType";
             break;   
+            case "&&":
+                w.type = "and";
+            break;
+            case "||":
+                w.type = "or";
+            break;
         }
     }
     
@@ -173,7 +190,7 @@ public class ProgLang {
         String regexVariables = "\\w*[a-zA-Z]\\w*";
         String regexString = "([\"'])(?:(?=(\\\\?))\\2.)*?\\1";
         String regexSOP = "System\\.out\\.println";
-        
+        String regexNotBoolVar = "!\\w*[a-zA-Z]\\w*";
        if (token.matches(regexNumber)) {
            w.type = "intValue";
        } else if (token.matches(regexVariables)) {
@@ -182,6 +199,74 @@ public class ProgLang {
            w.type = "stringValue";
        } else if (token.matches(regexSOP)) {
            w.type = "SOP";
+       } else if (token.matches(regexNotBoolVar)) {
+           w.type = "variable";
+       }
+    }
+    
+    
+    public static void checkCondition(LinkedList<Word> list) {
+       list.removeFirst();
+       list.removeLast();
+       boolean check1 , check2 , check3 , conditionSyntaxError;
+       check1 = false;
+       check2 = false;
+       check3 = false;
+       conditionSyntaxError = false;
+       if (list.size() == 0) {
+           System.out.println("Empty Conditions");
+           return;
+       }
+       for (int i = 0 ; i < list.size() ; i++) {
+           if (list.get(i).type.equals("variable")) {
+               check1 = true ;
+           }else if (list.get(i).type.equals("expression")) {
+               check2 = true;
+           }else if (list.get(i).type.equals("intValue")) {
+               check3 = true;
+           }
+           if (list.get(i).type.equals("and") || list.get(i).type.equals("or")) {
+               if(check1 && !check2 && !check3) {
+                   //System.out.println("Boolean variable");
+                   check1 = false;
+                   check2 = false;
+                   check3 = false;
+               } else if (check1 && check2 && check3) {
+                   //System.out.println("Comparative Statement");
+                   check1 = false;
+                   check2 = false;
+                   check3 = false;
+               } else {
+                   //System.out.println("Wrong conditions");
+                   conditionSyntaxError = true;
+                   check1 = false;
+                   check2 = false;
+                   check3 = false;
+               }
+           }
+       }
+       
+       if(check1 && !check2 && !check3) {
+                   //System.out.println("Boolean variable");
+                   check1 = false;
+                   check2 = false;
+                   check3 = false;
+               } else if (check1 && check2 && check3) {
+                   //System.out.println("Comparative Statement");
+                   check1 = false;
+                   check2 = false;
+                   check3 = false;
+               } else {
+                   //System.out.println("Wrong conditions");
+                   conditionSyntaxError = true;
+                   check1 = false;
+                   check2 = false;
+                   check3 = false;
+               }
+       if (conditionSyntaxError) {
+           System.out.println("Condition Syntax Error");
+       } else {
+           System.out.println("Condition correct");
        }
     }
 }
